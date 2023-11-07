48317f980497a7953d8003f0945b3d5416277e2a
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-11-06 23:02:34 -0300

    feat(CandidateEntity) adding some validations

--- src/main/java/br/com/bluelobster/vacancy_management/modules/canditate/CanditateEntity.java
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/canditate/CanditateEntity.java
@@ -2,14 +2,24 @@ package br.com.bluelobster.vacancy_management.modules.canditate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class CanditateEntity {
   private UUID id;
   private String name;
+
+  @Pattern(regexp = "^[^\\s]+$", message = "The [username] field must not contain spaces.")
   private String username;
+
+  @Email(message = "The [email] field must be a valid email")
   private String email;
+
+  @Length(min = 18, max = 256, message = "The password must have at least 18 characters.")
   private String password;
   private String description;
   private String curriculum;
--- src/main/java/br/com/bluelobster/vacancy_management/modules/canditate/controllers/CanditadeController.java
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/canditate/controllers/CanditadeController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bluelobster.vacancy_management.modules.canditate.CanditateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/canditate")
 public class CanditadeController {
   
   @PostMapping("/")
-  public void create(@RequestBody CanditateEntity canditateEntity) {
+  public void create(@Valid @RequestBody CanditateEntity canditateEntity) {
     System.out.println(canditateEntity);
   }
 }
