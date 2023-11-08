b7b1a2ce06a07922a41ce796285d692b560038c5
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-11-08 08:05:46 -0300

    feat(CandidateEntity): Decorate the Candidate with the @Entity annotation and related annotations.

--- src/main/java/br/com/bluelobster/vacancy_management/modules/canditate/CanditateEntity.java
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/canditate/CanditateEntity.java
@@ -1,16 +1,27 @@
 package br.com.bluelobster.vacancy_management.modules.canditate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CanditateEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
 
   @Pattern(regexp = "^[^\\s]+$", message = "The [username] field must not contain spaces.")
@@ -21,6 +32,11 @@ public class CanditateEntity {
 
   @Length(min = 18, max = 256, message = "The password must have at least 18 characters.")
   private String password;
+
   private String description;
+
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
