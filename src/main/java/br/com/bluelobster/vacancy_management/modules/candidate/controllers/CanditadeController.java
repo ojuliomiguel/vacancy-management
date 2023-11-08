748d3eb0dbf1843507c650f1ed3335c13103d644
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-11-08 08:25:46 -0300

    feat(CandidateController): saving candidate entity

---
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/candidate/controllers/CanditadeController.java
@@ -0,0 +1,28 @@
+package br.com.bluelobster.vacancy_management.modules.candidate.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bluelobster.vacancy_management.modules.candidate.CandidateEntity;
+import br.com.bluelobster.vacancy_management.modules.candidate.CandidateRepository;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/canditate")
+public class CanditadeController {
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  @PostMapping("/")
+  public ResponseEntity<CandidateEntity> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    this.candidateRepository.save(candidateEntity);
+
+    return new ResponseEntity<>(candidateEntity, HttpStatus.CREATED);
+  }
+}
