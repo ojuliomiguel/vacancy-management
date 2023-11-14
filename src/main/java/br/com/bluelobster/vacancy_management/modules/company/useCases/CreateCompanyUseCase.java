9ed1b9546668483345b9f7cf10b1f44e0b267eea
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-11-14 08:39:04 -0300

    fix(Company) typo error

--- src/main/java/br/com/bluelobster/vacancy_management/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,17 +1,19 @@
 package br.com.bluelobster.vacancy_management.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.bluelobster.vacancy_management.modules.company.entities.CompanyEntity;
 import br.com.bluelobster.vacancy_management.modules.company.repositories.CompanyRepository;
 import br.com.bluelobster.vacancy_management.modules.exceptions.UserAlreadyExistsException;
 
+@Service
 public class CreateCompanyUseCase {
 
   @Autowired
   private CompanyRepository companyRepository;
   
-  public CompanyEntity excecute(CompanyEntity companyEntity) {
+  public CompanyEntity execute(CompanyEntity companyEntity) {
     this.companyRepository
       .findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
       .ifPresent(user -> {
