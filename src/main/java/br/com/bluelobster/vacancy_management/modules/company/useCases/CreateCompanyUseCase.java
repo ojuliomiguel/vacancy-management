ebbd9e8ff23fa1f1125e3189d94ad7152a8dd444
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-11-21 08:04:24 -0300

    feat: encoding company password

--- src/main/java/br/com/bluelobster/vacancy_management/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,6 +1,7 @@
 package br.com.bluelobster.vacancy_management.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.bluelobster.vacancy_management.modules.company.entities.CompanyEntity;
@@ -12,6 +13,9 @@ public class CreateCompanyUseCase {
 
   @Autowired
   private CompanyRepository companyRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
   
   public CompanyEntity execute(CompanyEntity companyEntity) {
     this.companyRepository
@@ -19,6 +23,10 @@ public class CreateCompanyUseCase {
       .ifPresent(user -> {
         throw new UserAlreadyExistsException();
       });
+
+    var password = passwordEncoder.encode(companyEntity.getPassword());
+    companyEntity.setPassword(password);
+    
     return this.companyRepository.save(companyEntity);
   }
 
