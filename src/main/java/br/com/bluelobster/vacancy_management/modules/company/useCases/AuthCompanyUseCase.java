1ece1dd2ffee1ac3a40704c5f32a59511e0af409
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-11-21 08:22:31 -0300

    feat: auth company [WIP]

---
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,33 @@
+package br.com.bluelobster.vacancy_management.modules.company.useCases;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.com.bluelobster.vacancy_management.modules.company.dto.AuthCompanyDTO;
+import br.com.bluelobster.vacancy_management.modules.company.repositories.CompanyRepository;
+
+public class AuthCompanyUseCase {
+
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    var company = this.companyRepository
+      .findByUsername(authCompanyDTO.getUsername())
+      .orElseThrow(() -> {
+        throw new UsernameNotFoundException("Company not found");
+      });
+
+    var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+    if (!passwordMatches) {
+      throw new AuthenticationException();
+    }
+  }
+}
