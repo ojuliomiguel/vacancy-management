eb070eddba07920ea2af98ba25dd087683af531b
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-12-06 08:37:01 -0300

    feat(candidate) profile route [WIP]

--- src/main/java/br/com/bluelobster/vacancy_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,7 @@
 package br.com.bluelobster.vacancy_management.modules.candidate.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bluelobster.vacancy_management.modules.candidate.CandidateEntity;
 import br.com.bluelobster.vacancy_management.modules.candidate.useCases.CreateCanditateUseCase;
+import br.com.bluelobster.vacancy_management.modules.candidate.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/candidate")
@@ -18,6 +25,9 @@ public class CandidateController {
 
   @Autowired
   private CreateCanditateUseCase createCanditateUseCase;
+
+  @Autowired
+  private ProfileCandidateUseCase profileCandidateUseCase;
 
   @PostMapping("")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -27,6 +37,18 @@ public class CandidateController {
     } catch (Exception e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
     }
-    
   }
+
+  @GetMapping("")
+  public ResponseEntity<Object> get(HttpServletRequest request) {
+    var idCandidate = request.getAttribute("candidate_id");
+    try {
+      var profile = this.profileCandidateUseCase
+        .execute(UUID.fromString(idCandidate.toString()));
+      return ResponseEntity.ok().body(profile);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
 }
