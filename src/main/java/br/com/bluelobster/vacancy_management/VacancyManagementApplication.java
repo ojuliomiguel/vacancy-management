99df482699f04e41abfa977b6c28138ad5e094a3
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2024-01-03 08:07:23 -0300

    feat: setup swagger

--- src/main/java/br/com/bluelobster/vacancy_management/VacancyManagementApplication.java
+++ src/main/java/br/com/bluelobster/vacancy_management/VacancyManagementApplication.java
@@ -3,7 +3,17 @@ package br.com.bluelobster.vacancy_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Vacancy Management",
+		description = "Api designed to streamline the process of managing job vacancies.",
+		version = "1"
+	)
+)
 public class VacancyManagementApplication {
 
 	public static void main(String[] args) {
