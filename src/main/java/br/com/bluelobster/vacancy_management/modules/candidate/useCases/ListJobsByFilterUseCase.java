227c3f09b0ebba3f7a14aa18f17059ea89f9ce11
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2024-01-09 08:16:33 -0300

    refact(JobRepository): ignoring case when search for jobs name

--- src/main/java/br/com/bluelobster/vacancy_management/modules/candidate/useCases/ListJobsByFilterUseCase.java
+++ src/main/java/br/com/bluelobster/vacancy_management/modules/candidate/useCases/ListJobsByFilterUseCase.java
@@ -15,7 +15,7 @@ public class ListJobsByFilterUseCase {
     private JobRepository jobRepository;
 
     public List<JobEntity> execute(String filter) {
-        return this.jobRepository.findByDescriptionContaining(filter);
+        return this.jobRepository.findByDescriptionContainingIgnoreCase(filter);
     }
 
 }
